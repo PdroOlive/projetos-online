c337a37ae9342fffdc742376c6edf995ed518288
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-05-08 14:20:13 -0300

    Projeto atualizado

--- projeto-spotify/js/script.js
+++ projeto-spotify/js/script.js
@@ -1,25 +1,35 @@
 const playButton = document.getElementById("play-button");
 const song = document.getElementById("song");
+const imgAlbum = document.querySelector("#container-img img");
+const songName = document.getElementById("songName");
+const artist = document.getElementById("artist");
+const barUpdated = document.getElementById("container-bar-updated");
+const nextButton = document.getElementById("next-button");
+const backButton = document.getElementById("back-button");
+const barClicked = document.getElementById("container-bar");
+
+
 let play = false;
+let index = 0;
 
 const Mars = {
     songName: "That's What I Like",
-    artitst: "Bruno Mars",
-    estilo: "linear-gradient(to bottom, rgb(116, 1, 1), rgb(0, 0, 0) 110%)",
+    artist: "Bruno Mars",
+    style: "linear-gradient(to bottom, rgb(116, 1, 1), rgb(0, 0, 0) 110%)",
     file: "bruno-mars"
 }
 
 const Savage = {
     songName: "Glock in my Lap",
-    artitst: "21 Savage",
-    style:"",
+    artist: "21 Savage",
+    style:"linear-gradient(to bottom, rgb(214, 136, 33), rgb(0, 0, 0) 110%)",
     file: "savage"
 }
 
 const Fun ={
     songName: "We are Young",
-    artitst: "Fun",
-    style:"",
+    artist: "Fun",
+    style:"linear-gradient(to bottom, rgb(58, 0, 0), rgb(0, 0, 0) 110%)",
     file: "fun"
 }
 
@@ -51,4 +61,63 @@ function playPauseDecider()
     }
 }
 
+function loadingSong()
+{
+
+    imgAlbum.src = `images/${arraySong[index].file}.png`;
+    song.src = `songs/${arraySong[index].file}.mp3`;
+    songName.innerText = `${arraySong[index].songName}`
+    artist.innerText = `${arraySong[index].artist}`
+    document.body.style.backgroundImage = `${arraySong[index].style}`
+}
+
+function barProgress()
+{
+    const barWidth = (song.duration/song.currentTime)*100;
+    barUpdated.style.setProperty("--updated", `${barWidth}%`);
+}
+
+
+function nextSong()
+{
+    if(index === arraySong.length - 1)
+    {
+        index = 0;
+    }
+    else
+    {
+        index += 1;
+    }
+    loadingSong();
+    playSong();
+}
+
+function backSong()
+{
+    if(index === 0)
+    {
+        index = arraySong.length - 1;
+    }
+    else
+    {
+        index -= 1;
+    }
+    loadingSong();
+    playSong();
+}
+
+function JumpBar(event)
+{
+    const width = barClicked.clientWidth;
+    const clickPosition = event.offsetX;
+    const JumpTime = (clickPosition/width) * song.duration;
+    song.currentTime = JumpTime;
+}
+
+loadingSong();
+
 playButton.addEventListener("click", playPauseDecider);
+song.addEventListener("timeupdate", barProgress);
+nextButton.addEventListener("click", nextSong);
+backButton.addEventListener("click", backSong);
+barClicked.addEventListener("click", JumpBar);
