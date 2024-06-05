e18d224997cbf60f1431d82ddf6356d4e2361697
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-06-05 17:50:00 -0300

    update

--- curso-js002/project001/js/script.js
+++ curso-js002/project001/js/script.js
@@ -1,5 +1,20 @@
-window.addEventListener("keydown", (sound) =>
+function playSound(sound)
 {
-    const audio = document.querySelector(`audio[data-key="${sound.keyCode}"]`)
-    console.log(audio)
-});
+    const audio = document.querySelector(`audio[data-key="${sound.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${sound.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0; // rewind to the start
+    audio.play()
+    key.classList.add("is-clicked");
+}
+
+function removeTransition(e)
+{
+    if(e.propertyName !== "transform") return; // skip if it's not a transform
+    this.classList.remove("is-clicked");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition))
+
+window.addEventListener("keydown", playSound);
