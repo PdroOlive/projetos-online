6a768c7947550f866001df2303d93790ad9a9c8a
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-06-17 20:12:38 -0300

    update

--- santander-coders/project002/js/script.js
+++ santander-coders/project002/js/script.js
@@ -1,4 +1,27 @@
+const initSllider = () => 
+{
+    const imageList = document.querySelector(".container-movie");
+    const slideButtons = document.querySelectorAll(".container-mylist .button");
+
+    slideButtons.forEach(button =>
+    {
+        button.addEventListener("click", () =>
+        {
+            const direction = button.id === "arrow-back" ? -1 : 1;
+            const scrollAmount = 300 * direction;
+            imageList.scrollBy({left: scrollAmount, behavior: 'smooth'})
+        });
+    });
+
+    const checkSlider = () =>
+    {
+        if(imageList.scrollLeft !== 0) slideButtons[0].classList.add("show-effect");
+        else slideButtons[0].classList.remove("show-effect");
+    }
+    imageList.addEventListener("scroll", () => {checkSlider()});
+}
 
 
-
+document.querySelector(".button").addEventListener("click", () => {scrollIntoView({behavior: "smooth"});})
+window.addEventListener("load", initSllider);
 
