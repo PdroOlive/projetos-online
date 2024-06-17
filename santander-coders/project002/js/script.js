74f86198affa26a704a75caf78625bdf11b0ce6d
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-06-17 16:54:22 -0300

    uodate in project

--- santander-coders/project002/js/script.js
+++ santander-coders/project002/js/script.js
@@ -1,15 +1,4 @@
 
 
 
-function actionSlide(e)
-{
-    var slideMove = document.querySelector(".container-movie");
-    if(e == 1)
-    {
-        slideMove.scrollLeft = slideMove.scrollLeft - 200;
-    }
-    else if(e == 2)
-    {
-        slideMove.scrollLeft = slideMove.scrollLeft + 200;
-    }
-}
+
