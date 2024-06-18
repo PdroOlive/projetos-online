0ee5cc9cf4e9ee6442c72037b5f2059603e16166
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-06-18 19:30:48 -0300

    update all carousel

--- santander-coders/project002/js/script.js
+++ santander-coders/project002/js/script.js
@@ -1,27 +1,100 @@
+const slideTopten = document.querySelectorAll(".container-topten .topten-button")
+
 const initSllider = () => 
 {
     const imageList = document.querySelector(".container-movie");
-    const slideButtons = document.querySelectorAll(".container-mylist .button");
+    const slideMyList = document.querySelectorAll(".container-mylist .button");
 
-    slideButtons.forEach(button =>
+    slideMyList.forEach(button =>
     {
         button.addEventListener("click", () =>
         {
             const direction = button.id === "arrow-back" ? -1 : 1;
             const scrollAmount = 300 * direction;
-            imageList.scrollBy({left: scrollAmount, behavior: 'smooth'})
+            imageList.scrollBy({left: scrollAmount, behavior: 'smooth'});
         });
     });
 
     const checkSlider = () =>
     {
-        if(imageList.scrollLeft !== 0) slideButtons[0].classList.add("show-effect");
-        else slideButtons[0].classList.remove("show-effect");
+        if(imageList.scrollLeft !== 0) slideMyList[0].classList.add("show-effect");
+        else slideMyList[0].classList.remove("show-effect");
     }
     imageList.addEventListener("scroll", () => {checkSlider()});
 }
 
+const sliderTopTen = () => 
+{
+    const imageList = document.querySelector(".container-movie-topten");
+    const slideTopten = document.querySelectorAll(".container-topten .topten-button");
 
-document.querySelector(".button").addEventListener("click", () => {scrollIntoView({behavior: "smooth"});})
+    slideTopten.forEach(button =>
+    {
+        button.addEventListener("click", () =>
+        {
+            const direction = button.id === "prev-topten" ? -1 : 1;
+            const scrollAmount = 300 * direction;
+            imageList.scrollBy({left: scrollAmount, behavior: 'smooth'});
+        });
+    });
+
+    const checkSlider = () =>
+    {
+        if(imageList.scrollLeft !== 0) slideTopten[0].classList.add("show-effect");
+        else slideTopten[0].classList.remove("show-effect");
+    }
+    imageList.addEventListener("scroll", () => {checkSlider()});
+}
+
+const sliderForeign = () => 
+{
+    const imageList = document.querySelector(".container-movie-foreign");
+    const slideForeign = document.querySelectorAll(".container-foreign .foreign-button");
+
+    slideForeign.forEach(button =>
+    {
+        button.addEventListener("click", () =>
+        {
+            const direction = button.id === "prev-foreign" ? -1 : 1;
+            const scrollAmount = 300 * direction;
+            imageList.scrollBy({left: scrollAmount, behavior: 'smooth'});
+        });
+    });
+
+    const checkSlider = () =>
+    {
+        if(imageList.scrollLeft !== 0) slideForeign[0].classList.add("show-effect");
+        else slideForeign[0].classList.remove("show-effect");
+    }
+    imageList.addEventListener("scroll", () => {checkSlider()});
+}
+
+const sliderNewSeries = () => 
+{
+    const imageList = document.querySelector(".container-movie-newseries");
+    const slideNew = document.querySelectorAll(".container-newseries .newseries-button");
+
+    slideNew.forEach(button =>
+    {
+        button.addEventListener("click", () =>
+        {
+            const direction = button.id === "prev-new" ? -1 : 1;
+            const scrollAmount = 300 * direction;
+            imageList.scrollBy({left: scrollAmount, behavior: 'smooth'});
+        });
+    });
+
+    const checkSlider = () =>
+    {
+        if(imageList.scrollLeft !== 0) slideNew[0].classList.add("show-effect");
+        else slideNew[0].classList.remove("show-effect");
+    }
+    imageList.addEventListener("scroll", () => {checkSlider()});
+}
+    
+    
+
 window.addEventListener("load", initSllider);
-
+window.addEventListener("load", sliderTopTen);
+window.addEventListener("load", sliderForeign);
+window.addEventListener("load", sliderNewSeries);
