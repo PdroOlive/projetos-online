c8119cb82cd7f947e6a93149da101f4d179528b2
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-06-23 08:53:02 -0300

    aula01 js

--- santander-coders/project004/index.js
+++ santander-coders/project004/index.js
@@ -1,1 +1,7 @@
-console.log("Hello World!!")
+const prompt = require("readline-sync")
+
+
+let nameOfPerson = prompt.question("Qual o seu nome?"); // Camel case
+let age = Number(prompt.question("Digite sua idade:"));
+
+console.log(typeof(age))
