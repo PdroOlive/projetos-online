8acfade41ef2601e0e1808cb0e291e52f95a3d26
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-06-29 17:36:24 -0300

    Update jason.js

--- santander-coders/project004/jason.js
+++ santander-coders/project004/jason.js
@@ -6,7 +6,7 @@ const file = fs.readFile("./santander-coders/project004/file.json", (erro, ok) =
     else
     {
         const obj = JSON.parse(ok); //JSON.parse converte os dados:Buffer do Json para object do JS
-        console.log(JSON.stringify(obj));  //JSON.stringify converte os dados JS para JSON.
+        console.log(JSON.stringify(obj));  //JSON.stringify converte os dados JS para JSON. 
     } 
 });
 
