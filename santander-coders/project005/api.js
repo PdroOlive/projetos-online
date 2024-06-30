c57beae8d0cf7c58bc527946116206f6c1a298ee
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-06-30 13:07:45 -0300

    update

--- santander-coders/project005/api.js
+++ santander-coders/project005/api.js
@@ -1,4 +1,32 @@
-fetch("https://viacep.com.br/ws/66020445/json/").then((response) =>
+// fetch("https://viacep.com.br/ws/66020445/json/").then((response) =>
+// {
+//     response.json().then((data) => console.log(data))
+
+// }).catch((error) =>
+// {
+//     console.log(error);
+// }).finally(() =>
+// {
+//     console.log("Requisição  Finalizada!")
+// })
+
+async function viaCep()
 {
-    
-})
+    try
+    {
+        const zipCode = await fetch("https://viacep.com.br/ws/66020445/json/");
+        const dataZipCode = await zipCode.json();
+        console.log(dataZipCode);
+    }
+    catch(error)
+    {
+        console.log(error);
+
+    }
+    finally
+    {
+        console.log("Requisição Finalizada!!")
+    }
+}
+
+viaCep()
