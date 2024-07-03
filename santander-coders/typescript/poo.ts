ee53f0eae52ee39787322d63772a1f15edc9b335
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-07-03 17:07:42 -0300

    update

--- santander-coders/typescript/poo.ts
+++ santander-coders/typescript/poo.ts
@@ -1,12 +1,36 @@
-class Pessoa
+interface IPerson 
 {
     nome: string;
     idade: number;
-    cpf: number;
+    cpf: string;
     conta: number;
+
+    dormir
+}
+
+
+class Pessoa implements IPerson
+{
+    nome: string;
+    idade: number;
+    cpf: string;
+    conta: number;
+    
+
+    constructor(nome: string, idade: number, cpf: string, conta: number)
+    {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf; 
+        this.conta = conta;
+    }
 
     dormir() 
     {
         console.log("Dormindo....😴😴")
     }
 }
+
+const personOne = new Pessoa("Pedro", 20, "334.434.233-90", 4345);
+
+console.log(personOne.dormir());
