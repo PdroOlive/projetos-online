84a929962de83bc4ca8d07df75b8881da215affe
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-07-04 20:42:02 -0300

    update

--- santander-coders/api-browsers/index.js
+++ santander-coders/api-browsers/index.js
@@ -1,3 +1,5 @@
 const titleMain = document.getElementById("title-main");
+const button = document.getElementById("button");
 
+button.addEventListener("click", (event) => console.log("Clicou", event))
 titleMain.style.color = `red`;
