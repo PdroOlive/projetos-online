cc580e95c6cf1bb391f95931a3b1b9e52427cd82
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-07-08 13:55:59 -0300

    update

--- santander-coders/api-browsers/index.js
+++ santander-coders/api-browsers/index.js
@@ -2,7 +2,10 @@ const titleMain = document.getElementById("title-main");
 const buttonMin = document.getElementById("button-min");
 const buttonMax = document.getElementById("button-max");
 const inputElement = document.getElementById("iinput");
+const themeSwitch = document.getElementById("switch-theme");
 const cont = document.getElementById("cont");
+let controlTheme;
+
 
 buttonMin.addEventListener("click", () => 
 {
@@ -14,5 +17,34 @@ buttonMax.addEventListener("click", () =>
     const valueActual = Number(cont.innerHTML);
     cont.innerHTML = valueActual + 1;
 });
+
+const themeRenew = () =>
+{
+    if(controlTheme) 
+    { 
+        document.body.style.background = "rgb(87, 87, 87)";
+        document.body.style.color = `white`;
+    }
+    else
+    {
+        document.body.style.background = "white";
+        document.body.style.color = `black`;
+    }
+}
+
+window.onload = () =>
+{
+    const themeInStorage = localStorage.getItem("theme");
+    controlTheme = themeInStorage === "true";
+    themeRenew();
+}
+
+themeSwitch.addEventListener("click", () =>
+{
+    controlTheme = !controlTheme;
+
+    localStorage.setItem("theme", controlTheme);
+    themeRenew();
+})
 
 inputElement.addEventListener("input", () => console.log(inputElement.value))
---
+++ santander-coders/task-list/script.js
@@ -0,0 +1,20 @@
+const formField = document.getElementById("container-form");
+const addTaskInput = document.getElementById("add-task");
+const addTaskButton = document.getElementById("apply-add-task");
+const listField = document.getElementById("container-list");
+
+addTaskButton.addEventListener("click", () => 
+{
+    if(addTaskInput.value === "")
+        alert("Informe sua tarefa!!!")
+    else
+    {
+        inputValue = addTaskInput.value;
+        const newTagList = document.createElement("li");
+        newTagList.innerHTML = inputValue;
+        listField.appendChild(newTagList);
+        addTaskInput.value = "";
+    }
+    
+});
+formField.onsubmit = () => false;
