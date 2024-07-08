7dab62ba4ae9985c070661855807fb3314567f30
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-07-08 16:28:54 -0300

    update

--- santander-coders/task-list/script.js
+++ santander-coders/task-list/script.js
@@ -3,18 +3,34 @@ const addTaskInput = document.getElementById("add-task");
 const addTaskButton = document.getElementById("apply-add-task");
 const listField = document.getElementById("container-list");
 
-addTaskButton.addEventListener("click", () => 
+const genericFun = () =>
 {
     if(addTaskInput.value === "")
-        alert("Informe sua tarefa!!!")
-    else
-    {
-        inputValue = addTaskInput.value;
-        const newTagList = document.createElement("li");
-        newTagList.innerHTML = inputValue;
-        listField.appendChild(newTagList);
-        addTaskInput.value = "";
-    }
-    
-});
-formField.onsubmit = () => false;
+         alert("Informe sua tarefa!!!")
+     else
+     {
+         inputValue = addTaskInput.value;
+         const newTagList = document.createElement("li");
+         newTagList.innerHTML = inputValue;
+         listField.appendChild(newTagList);
+         addTaskInput.value = "";
+     }
+}
+formField.onsubmit = () => 
+{
+    genericFun()    
+    return false;
+}
+// addTaskButton.addEventListener("click", () => 
+// {
+//     if(addTaskInput.value === "")
+//         alert("Informe sua tarefa!!!")
+//     else
+//     {
+//         inputValue = addTaskInput.value;
+//         const newTagList = document.createElement("li");
+//         newTagList.innerHTML = inputValue;
+//         listField.appendChild(newTagList);
+//         addTaskInput.value = "";
+//     }
+// });
